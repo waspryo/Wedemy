2272a4d6dd1571ffd92e461982ffef21efaec875
waspryo <wasp2123321@gmail.com>
2018-12-30 19:10:06 +0900

    improve

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  def top    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :videos
   get 'pages/about'
   get 'pages/contact'
-  root 'videos#index'
+  get 'pages/top'
+  root 'pages#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
